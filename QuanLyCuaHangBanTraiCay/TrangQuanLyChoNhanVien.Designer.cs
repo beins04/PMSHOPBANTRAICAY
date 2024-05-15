6632a8f9bb42a6fd4a8a647f5f4907314e83776c
Trung Kiên Trần Nguyễn <cristntk0162@gmail.com>
2024-05-15 17:53:15 +0700

    Kien done

--- QuanLyCuaHangBanTraiCay/TrangQuanLyChoNhanVien.Designer.cs
+++ QuanLyCuaHangBanTraiCay/TrangQuanLyChoNhanVien.Designer.cs
@@ -168,6 +168,7 @@
             this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox2.TabIndex = 19;
             this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
             // 
             // TrangQuanLyChoNhanVien
             // 
--- QuanLyCuaHangBanTraiCay/TrangQuanLyChoNhanVien.cs
+++ QuanLyCuaHangBanTraiCay/TrangQuanLyChoNhanVien.cs
@@ -110,5 +110,10 @@ namespace QuanLyCuaHangBanTraiCay
         {
 
         }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
